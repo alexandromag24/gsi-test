07dc3670c07bee6baf139177317cb5e9bb38fbad
alexandromag24 <34452436+alexandromag24@users.noreply.github.com>
2021-09-22 14:55:38 -0500

    changed environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: true,
-  PARSE_APP_ID: 'dbbuttymanag',
-  PARSE_JS_KEY: '88030911379',
-  serverURL: 'https://dbbuttymanag.herokuapp.com/parse'
+  PARSE_APP_ID: 'buttybd8803',
+  PARSE_JS_KEY: '880309113002021',
+  serverURL: 'https://buttyparse.herokuapp.com/parse'
 };
 
 // -- DEV
